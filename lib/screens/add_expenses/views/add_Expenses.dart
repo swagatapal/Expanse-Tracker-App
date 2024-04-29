d7f88338ae9f4aa966c854c19a8d5ae9bf76838e
swagata pal <swagatapal1980@gmail.com>
2024-04-29 18:10:59 +0530

    add expanse page design

--- lib/screens/add_expenses/views/add_Expenses.dart
+++ lib/screens/add_expenses/views/add_Expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -8,18 +10,285 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  DateTime selectDate = DateTime.now();
+  List<String> myCategoriesIcons = [
+    'entertainment',
+    'food',
+    'home',
+    'pet',
+    'shopping',
+    'tech',
+    'travel'
+  ];
+
+  @override
+  void initState() {
+    dateController.text =
+        DateFormat('dd/MM/yyyy').format(DateTime.now()).toString();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Add Expenses"),
-        ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Add Expenses",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  controller: expenseController,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              TextFormField(
+                readOnly: true,
+                controller: categoryController,
+                onTap: () {},
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.list,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (ctx) {
+                              bool isExpanded = false;
+                              return StatefulBuilder(
+                                  builder: (context, setState) {
+                                return AlertDialog(
+                                  title: const Text("Create a category"),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextFormField(
+                                        // readOnly: true,
+                                        // controller: dateController,
+
+                                        textAlignVertical:
+                                            TextAlignVertical.center,
+                                        onTap: () async {},
+                                        decoration: InputDecoration(
+                                          filled: true,
+                                          isDense: true,
+                                          fillColor: Colors.white,
+                                          prefixIcon: const Icon(
+                                            FontAwesomeIcons.list,
+                                            size: 16,
+                                            color: Colors.grey,
+                                          ),
+                                          hintText: "Name",
+                                          border: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12),
+                                              borderSide: BorderSide.none),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 16,
+                                      ),
+                                      TextFormField(
+                                        readOnly: true,
+                                        // controller: dateController,
+                                        textAlignVertical:
+                                            TextAlignVertical.center,
+                                        onTap: () async {
+                                          setState(() {
+                                            isExpanded = !isExpanded;
+                                          });
+                                        },
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          prefixIcon: const Icon(
+                                            FontAwesomeIcons.icons,
+                                            size: 16,
+                                            color: Colors.grey,
+                                          ),
+                                          suffixIcon: const Icon(
+                                            Icons.arrow_drop_down_sharp,
+                                            size: 40,
+                                          ),
+                                          hintText: "Icon",
+                                          border: OutlineInputBorder(
+                                              borderRadius: isExpanded
+                                                  ? BorderRadius.vertical(
+                                                      top: Radius.circular(12),
+                                                    )
+                                                  : BorderRadius.circular(12),
+                                              borderSide: BorderSide.none),
+                                        ),
+                                      ),
+                                      isExpanded
+                                          ? Container(
+                                              width: MediaQuery.of(context)
+                                                  .size
+                                                  .width,
+                                              height: 200,
+                                              decoration: const BoxDecoration(
+                                                  color: Colors.white,
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                    bottomLeft:
+                                                        Radius.circular(12),
+                                                    bottomRight:
+                                                        Radius.circular(12),
+                                                  )),
+                                              child: GridView.builder(
+                                                  gridDelegate:
+                                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                                          crossAxisCount: 3),
+                                                  itemCount:
+                                                      myCategoriesIcons.length,
+                                                  itemBuilder:
+                                                      (context, int i) {
+                                                    return Container(
+                                                      height: 50,
+                                                      width: 50,
+                                                      decoration: BoxDecoration(
+                                                          image: DecorationImage(
+                                                              image: AssetImage(
+                                                                  'assets/${myCategoriesIcons[i]}.png'))),
+                                                    );
+                                                  }),
+                                            )
+                                          : Container(),
+                                      const SizedBox(
+                                        height: 16,
+                                      ),
+                                      TextFormField(
+                                        // readOnly: true,
+                                        // controller: dateController,
+                                        textAlignVertical:
+                                            TextAlignVertical.center,
+                                        onTap: () async {},
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          prefixIcon: const Icon(
+                                            FontAwesomeIcons.faceAngry,
+                                            size: 16,
+                                            color: Colors.grey,
+                                          ),
+                                          hintText: "Color",
+                                          border: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12),
+                                              borderSide: BorderSide.none),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                            });
+                      },
+                      icon: const Icon(
+                        FontAwesomeIcons.plus,
+                        size: 16,
+                        color: Colors.grey,
+                      )),
+                  hintText: "Category",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                readOnly: true,
+                controller: dateController,
+                textAlignVertical: TextAlignVertical.center,
+                onTap: () async {
+                  DateTime? newDate = await showDatePicker(
+                      context: context,
+                      initialDate: selectDate,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(const Duration(days: 365)));
+                  if (newDate != null) {
+                    setState(() {
+                      dateController.text =
+                          DateFormat('dd/MM/yyyy').format(newDate).toString();
+                      selectDate = newDate;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.clock,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: "Date",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  child: const Text("save",
+                      style: TextStyle(fontSize: 22, color: Colors.white)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/stats/stats_screen.dart
+++ lib/screens/stats/stats_screen.dart
@@ -1,5 +1,6 @@
 import 'package:expanse_tracker/screens/stats/chart.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StatsScreen extends StatelessWidget {
   const StatsScreen({super.key});
@@ -7,35 +8,52 @@ class StatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
-      child: Padding(
-        padding:  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
-        child: Column(
-          children: [
-            const Text(
-              'Transactions',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+          child: Column(
+            children: [
+              const Text(
+                'Transactions',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-             Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12)
-              ),
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
-                child: MyChart(),
+              const SizedBox(height: 20),
+               Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+                  child: MyChart(),
+                )
+               ),
+              const SizedBox(height: 10,),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+                    child: SfCartesianChart(),
+                  )
               )
-             )
-
-          ],
-          
+        
+            ],
+            
+          ),
         ),
       ),
     );
   }
 }
+
+
